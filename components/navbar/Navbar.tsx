b73f0526b2650cb5f58be172b9f0fe7907afeedb
heatyux <421743974@qq.com>
2025-07-16 14:41:21 +0800

    navbar & container added

---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+import CartButton from "./CartButton";
+import Container from "./Container";
+import Logo from "./Logo";
+import NavSearch from "./NavSearch";
+
+const Navbar = () => {
+  return (
+    <nav className="border-b">
+      <Container className="flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap gap-4 py-8">
+        <Logo />
+        <NavSearch />
+        <div className="flex gap-4 items-center">
+          <CartButton />
+          {/* <DarkMode /> */}
+          {/* <LinksDropdown /> */}
+        </div>
+      </Container>
+    </nav>
+  );
+};
+
+export default Navbar;
